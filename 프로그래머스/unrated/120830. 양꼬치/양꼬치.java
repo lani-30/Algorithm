b807b2789f3267e92bbf6026c9d1c4d052d342c9
Yerina <122720744+lani-30@users.noreply.github.com>
2024-01-10 10:04:49 +0900

    [level unrated] Title: 양꼬치, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+          
+        
+        return answer = (12000 * n) + (2000 * (k-(n/10)));
+    }
+}
