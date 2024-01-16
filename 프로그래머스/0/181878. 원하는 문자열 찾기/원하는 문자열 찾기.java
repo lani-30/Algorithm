90f0dd4bc58ecc5742b1b9ffa12d1da7d8293eaf
Yerina <122720744+lani-30@users.noreply.github.com>
2024-01-17 08:58:37 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 5.06 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        
+        
+        return (myString.toUpperCase()).contains((pat.toUpperCase())) ? 1 : 0;
+    }
+}
