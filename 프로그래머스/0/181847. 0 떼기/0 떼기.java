603322256b818aa045742e16cb88d3f64e1d38bb
Yerina <122720744+lani-30@users.noreply.github.com>
2024-01-17 11:51:46 +0900

    [level 0] Title: 0 떼기, Time: 11.52 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String solution(String n_str) {
+        
+        String answer = "";
+	        int idx = 0;
+	        
+	        for(int i=0; i<n_str.length(); i++) {
+	        	if(n_str.charAt(i) == '0') answer += "";
+	        	else {
+	        		idx = i;
+	        		break;
+	        	} 
+	        }
+	        
+	        for(int i = idx; i<n_str.length(); i++) {
+	        	answer += n_str.charAt(i);
+	        }
+        
+        return answer;
+    }
+}
