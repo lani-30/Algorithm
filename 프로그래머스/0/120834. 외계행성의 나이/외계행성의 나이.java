2b0df9112d0b3595fe311dcc66dcc491e25c14b8
Yerina <122720744+lani-30@users.noreply.github.com>
2024-01-19 09:46:10 +0900

    [level 0] Title: 외계행성의 나이, Time: 2.53 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int age) {
+        String answer = "";
+        String ageString = String.valueOf(age);
+        
+        for(int i=0; i<ageString.length(); i++) {
+        	
+        	switch(ageString.charAt(i)){
+        	case '0' : answer += "a"; break;
+        	case '1' : answer += "b";  break;
+        	case '2' : answer += "c";  break;
+        	case '3' : answer += "d";  break;
+        	case '4' : answer += "e";  break;
+        	case '5' : answer += "f";  break;
+        	case '6' : answer += "g";  break;
+        	case '7' : answer += "h";  break;
+        	case '8' : answer += "i";  break;
+        	case '9' : answer += "j";  break;
+        	}
+        }
+        
+        return answer;
+    }
+}
