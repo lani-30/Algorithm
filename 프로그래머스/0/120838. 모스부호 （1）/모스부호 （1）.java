626e6387b35e3970a9b4a0d80446627501767fa3
Yerina <122720744+lani-30@users.noreply.github.com>
2024-01-23 11:06:39 +0900

    [level 0] Title: 모스부호 (1), Time: 1.44 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String letter) {
+        String answer = "";
+        
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+	        String[] alpha = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+	        
+	        String[] letters = letter.split(" ");
+	        
+	        for(int i=0; i<letters.length; i++){
+	            
+	        	for(int j=0; j<morse.length; j++) {
+	        		
+	        		if(morse[j].equals(letters[i])) {
+	        			answer += alpha[j];
+	        		}
+	        	}
+	        }
+        
+        return answer;
+    }
+}
