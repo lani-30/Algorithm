b2e8b712618b4b20d79bd5ce661c8233e5a392f8
Yerina Lee <122720744+yerinaLee@users.noreply.github.com>
2024-03-21 13:20:38 +0900

    [Bronze III] Title: 최댓값, Time: 140 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int[] arr = new int[9];
+		int num = 0;
+		int max = 0;
+		
+		for(int i=0; i<9; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			
+			if(max < arr[i]) {
+				max = arr[i];
+				num = i+1;
+			}
+		}
+		
+		bw.write(max+"\n");
+		bw.write(num+"\n");
+		
+		br.close();
+		bw.close();
+	}
+}
