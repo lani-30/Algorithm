ea4efb222be05886401986343a9d63b43246e061
Yerina Lee <122720744+yerinaLee@users.noreply.github.com>
2024-03-29 10:01:56 +0900

    [Bronze II] Title: 단어의 개수, Time: 280 ms, Memory: 25448 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String str = br.readLine();
+		
+		String[] arr = str.split(" ");
+		
+		if(str.equals(" ")) bw.write(arr.length + "");
+		
+		else if(arr[0].equals("")) {
+			List<String> list = new ArrayList<String>(Arrays.asList(arr));
+			list.remove(0);
+			bw.write(list.size() +"");
+			
+		} else {
+			bw.write(arr.length + "");
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
