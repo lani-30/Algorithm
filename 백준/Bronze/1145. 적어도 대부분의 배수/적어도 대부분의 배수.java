08a6aa07f13b8a15e16ef7c5f8488ebfe9daaed0
Yerina Lee <122720744+yerinaLee@users.noreply.github.com>
2024-04-08 09:49:08 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 176 ms, Memory: 17080 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[5];
+		for(int i=0; i<5; i++) arr[i] = Integer.parseInt(st.nextToken());
+		
+		int times = 0;
+		int answer = 0;
+		
+		for(int i=1; ; i++) {
+			
+			times = 0;
+			
+			for(int j=0; j<5; j++) {
+				if(i % arr[j] == 0) times++; 
+			}
+			
+			if(times >= 3) {
+				answer = i; break;
+			}
+		}
+		
+		bw.write(answer+"");
+		
+		br.close();
+		bw.close();
+	}
+}
