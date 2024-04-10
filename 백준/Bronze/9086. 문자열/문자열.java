4e1253b8ffd7b80766718abd5684d8112cd9dc17
Yerina Lee <122720744+yerinaLee@users.noreply.github.com>
2024-04-10 09:33:27 +0900

    [Bronze V] Title: 문자열, Time: 144 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<num; i++) {
+			String str = br.readLine();
+			bw.write(str.charAt(0)+""+str.charAt(str.length()-1)+"\n");
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
