59fb0f2bc167aa368b3cdd094ca65ebb31fe0794
Yerina Lee <122720744+yerinaLee@users.noreply.github.com>
2024-04-22 09:41:53 +0900

    [Bronze I] Title: 임시 반장 정하기, Time: 184 ms, Memory: 15740 KB -BaekjoonHub

---
+++ 백준/Bronze/1268. 임시 반장 정하기/임시 반장 정하기.java
@@ -0,0 +1,55 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		int[][] arr = new int[num][5];
+		
+		for(int i=0; i<num; i++) {
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			for(int j=0; j<5; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+
+
+		int[] answer = new int[num];
+
+		for(int i=0; i<num; i++) {
+			
+			for(int row=0; row<answer.length; row++) {
+				for(int col=0; col<5; col++) {
+					if(arr[i][col] == arr[row][col]) {
+						answer[i]++;
+						break;
+					}
+				}
+			}
+		}
+		
+		
+		
+		int max = answer[0];
+		
+		for(int i : answer) max = Math.max(max, i);
+		
+		for(int i=0; i<answer.length; i++) {
+			if(answer[i] == max) {
+				bw.write(i+1+"");
+				break;
+			}
+		}
+		
+		br.close();
+		bw.close();
+	}
+}
