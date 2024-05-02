a0e4b661e8df09f341f89b20c31871e0c4cded5b
Yerina Lee <122720744+yerinaLee@users.noreply.github.com>
2024-05-02 21:49:52 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,27 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		List<Integer> list = new ArrayList<Integer>();
+		
+		String str = br.readLine();
+		
+		for(int i=0; i<str.length(); i++) {
+			list.add(Integer.parseInt(String.valueOf(str.charAt(i))));
+		}
+		
+		list.sort(Comparator.reverseOrder());
+		
+		for(int i:list) bw.write(i+"");
+		
+		br.close();
+		bw.close();
+	}
+}
