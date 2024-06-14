28b8478317a2934115453ca43c3d7ad0c6c815dc
Yerina Lee <122720744+yerinaLee@users.noreply.github.com>
2024-06-14 22:42:31 +0900

    [Bronze V] Title: 사파리월드, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,32 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		long n = Integer.parseInt(st.nextToken());
+		long m = Integer.parseInt(st.nextToken());
+		
+		long sum = n-m;
+		
+		if(sum < 0) sum *= -1;
+		
+		bw.write(sum+"");
+			
+			
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
+
+
+
+
